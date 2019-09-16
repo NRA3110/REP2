13ba87b2bf66daca387b527c9bc76b68878a7082
nickranderson <nickranderson@cb5109-57.labs.sce.carleton.ca>
2019-09-16 12:10:27 -0400

    I added an output to the class AddressBook.java to test GIT commit.

---
+++ src/BuddyInfoPackage/AddressBook.java
@@ -0,0 +1,27 @@
+package BuddyInfoPackage;
+
+import java.util.ArrayList;
+
+public class AddressBook
+{
+	public static ArrayList<BuddyInfo> buddies;
+	
+	void addBuddy(BuddyInfo buddy)
+	{
+		buddies.add(buddy);
+	}
+	void removeBuddy(BuddyInfo buddy)
+	{
+		buddies.remove(buddy);
+	}
+	
+	
+	public static void main2(String[] args)
+	{
+		BuddyInfo Jeff = new BuddyInfo();
+		Jeff.setName("Jeff");
+		AddressBook buddyBook = new AddressBook();
+		buddyBook.addBuddy(Jeff);
+		System.out.println(AddressBook.buddies);
+	}
+}
